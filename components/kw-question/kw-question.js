628aa0c7cbd281b377cf7ea41875ec925509d483
kevinwestern <kevinwestern@gmail.com>
2015-07-14 22:27:30 -0700

    Adds support for adding comments. Need to clean up UI and figure out why comments are displaying after submitting.

--- components/kw-question/kw-question.js
+++ components/kw-question/kw-question.js
@@ -2,10 +2,17 @@ Polymer({
   is: "kw-question",
 
   properties: {
-    question: Object,
+    question: {
+      type: Object,
+      observer: 'questionChanged_'
+    },
     computedVotes: {
       type: Number,
       computed: 'getComputedVotes_(question)'
+    },
+    comments: {
+      type: Array,
+      computed: 'getComments_(question.comments)'
     }
   },
 
@@ -21,8 +28,29 @@ Polymer({
     return this.question.votes + question.vote;
   },
 
+  getComments_: function(comments) {
+    console.log(comments)
+    return comments;
+  },
+
   getClassNames: function(question, thumb) {
     return ((question.vote == 1 && thumb == 'up')
       || (question.vote == -1 && thumb == 'down')) ? 'selected' : '';
+  },
+
+  showCommentBox: function() {
+    Polymer.dom(this.$.commentBox).classList.toggle('hidden');
+  },
+
+  submitComment: function() {
+    this.fire('submitComment', {
+      commentText: this.$.comment.value,
+      question: this.question
+    });
+  },
+
+  questionChanged_: function() {
+    this.$.comment.value = '';
+    Polymer.dom(this.$.commentBox).classList.toggle('hidden', true);
   }
 });
--- es6/models/Question.js
+++ es6/models/Question.js
@@ -1,25 +1,31 @@
 export class Question {
-  constructor(text, votes, opt_vote, opt_key) {
+  constructor(text, votes, opt_vote, opt_key, opt_comments) {
     this.text = text;
     this.votes = votes;
     this.vote = (opt_vote == undefined) ? 1 : opt_vote;
     this.key = opt_key;
+    this.comments = opt_comments || [];
   }
 
   upVote() {
     if (this.vote == 1) {
-      return new Question(this.text, this.votes, 0, this.key);
+      return new Question(this.text, this.votes, 0, this.key, this.comments);
     } else {
-      return new Question(this.text, this.votes, 1, this.key);
+      return new Question(this.text, this.votes, 1, this.key, this.comments);
     }
   }
 
   downVote() {
     if (this.vote == -1) {
-      return new Question(this.text, this.votes, 0, this.key);
+      return new Question(this.text, this.votes, 0, this.key, this.comments);
     } else {
-      return new Question(this.text, this.votes, -1, this.key);
+      return new Question(this.text, this.votes, -1, this.key, this.comments);
     }
+  }
+
+  addComment(text) {
+    this.comments.push(text);
+    return new Question(this.text, this.votes, this.vote, this.key, this.comments);
   }
 
   setKey(key) {
@@ -27,6 +33,6 @@ export class Question {
   }
 
   static fromJSON(json, opt_key) {
-    return new Question(json.text, json.votes, json.vote, opt_key);
+    return new Question(json.text, json.votes, json.vote, opt_key, json.comments);
   }
 };
